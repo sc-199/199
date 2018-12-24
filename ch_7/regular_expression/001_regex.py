71fb5ae6dce1c0204f1afd48666c23c94221ebc4
hermano72 <paate72@yahoo.com>
2018-12-24 13:11:51 +0400

    Create 001_regex.py

---
+++ ch_7/regular_expression/001_regex.py
@@ -0,0 +1,12 @@
+import re
+
+phone_number_regex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
+mo = phone_number_regex.search("My phone number is 415-555-4242")
+
+print("Found phone number:", mo.group())
+
+
+
+
+# ----------------------
+input("\nDone!..")
