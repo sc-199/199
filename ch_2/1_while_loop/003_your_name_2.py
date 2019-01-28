e8288b273cd1833eb50bea017e1a96d2ed7a3804
pm72 <pm72github@yahoo.com>
2019-01-28 12:50:00 +0400

    Create 003_your_name_2.py

---
+++ ch_2/1_while_loop/003_your_name_2.py
@@ -0,0 +1,18 @@
+""" break ინსტრუქცია """
+
+while True:
+	print("Please type your name.")
+	name = input()
+	
+	print()
+	
+	if name == "your name":
+		break
+
+print("Thank you!")
+
+
+
+
+# -----------------
+input("\nDone!..")
