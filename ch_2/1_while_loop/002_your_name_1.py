8acd8a71aa87aa438f873e2bf8136f2330465aeb
pm72 <pm72github@yahoo.com>
2019-01-28 12:50:10 +0400

    Create 002_your_name_1.py

---
+++ ch_2/1_while_loop/002_your_name_1.py
@@ -0,0 +1,15 @@
+""" ჭირვეული while ციკლი """
+
+name = ''
+while name != "your name":
+	print("Please type your name.")
+	name = input()
+	print()
+
+print("Thank you!")
+
+
+
+
+# -----------------
+input("\nDone!..")
