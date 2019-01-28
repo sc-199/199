8fd10d734684111546b30548ee55aadba3bd4155
pm72 <pm72github@yahoo.com>
2019-01-28 13:35:09 +0400

    Create 005_True_False.py

---
+++ ch_2/1_while_loop/005_True_False.py
@@ -0,0 +1,17 @@
+name = ""
+
+while not name:
+  print("Enter your name:")
+  name = input()
+
+print("\nHow many guests will you have?")
+num_of_guests = int(input())
+
+if num_of_guests:
+  print("\nBe sure to have enough room for all your guests.")
+
+
+
+
+# ------------------
+input("\nDone!..")
