403d03de3a1ae697dad593d3caed0c7822e9a7ab
pm72 <pm72github@yahoo.com>
2019-02-04 17:54:19 +0400

    Update found_max.py

--- ch_3 (functions)/found_max.py
+++ ch_3 (functions)/found_max.py
@@ -12,6 +12,20 @@ def max2(x, y):
   # return y
 
 
+def max3(x, y, z):
+  return max2(x, max2(y,z))
+
+
+
+# ------------------
+print(max2(4, 9))
+print(max3(5, 2, 7))
+print(max3(4.69, 5.41, 1.02))
+print(max3(4.69, 5.41, 17))
+print(max3("ab", "abc", "abd"))       # Duck typing
+                                      # იხვისებრი პოლიმორფიზმი
+                                      # იხვისებრი ტიპიზაცია
+
 
 
 # ----------------
