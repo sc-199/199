e09811a191a5b66dab98afe63b1d2c818cbd6503
pm72 <pm72github@yahoo.com>
2019-02-04 17:37:39 +0400

    Create 002_hello.py

---
+++ ch_3 (functions)/002_hello.py
@@ -0,0 +1,19 @@
+'''ფუნქცია პარამეტრებით'''
+
+# def hello(name):
+#   print("Hello,", name)
+
+def hello(name="world!.."):
+  print("Hello,", name)
+
+
+# -------------------
+# hello("Paata")
+hello()
+hello("John")
+
+
+
+
+# -----------------
+input("\nDone!..")
