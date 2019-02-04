5f5d84bc8dc0b8b45ff29d43d10a8795e040b049
pm72 <pm72github@yahoo.com>
2019-02-04 17:37:42 +0400

    Create found_max.py

---
+++ ch_3 (functions)/found_max.py
@@ -0,0 +1,18 @@
+''' მაქსიმუმის პოვნა '''
+
+def max2(x, y):
+  if x > y:
+    return x
+  else:
+    return y
+  
+  # if x > y:
+  #   return x
+  
+  # return y
+
+
+
+
+# ----------------
+input("\nDone!..")
