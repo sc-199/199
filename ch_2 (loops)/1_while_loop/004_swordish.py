dc75d7d2da63c87262478d4bedd9164948f6a0b7
pm72 <pm72github@yahoo.com>
2019-02-05 13:10:19 +0400

    Create 004_swordish.py

---
+++ ch_2 (loops)/1_while_loop/004_swordish.py
@@ -0,0 +1,22 @@
+while True:
+  print("Who are you?")
+  name = input()
+  print()
+
+  if name != "Joe":
+    continue
+  
+  print("Hello Joe. What is the password? (It is a fish.)")
+  password = input()
+  print()
+
+  if password == "swordfish":
+    break
+
+print("Access granted.")
+
+
+
+
+# ---------------------------
+input("\nDone!..")
