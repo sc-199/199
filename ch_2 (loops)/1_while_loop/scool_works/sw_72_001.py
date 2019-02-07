ad9f3ff59ba79fdb78e26a52526c3872abcd1066
pm72 <pm72github@yahoo.com>
2019-02-07 12:41:39 +0400

    Update sw_72_001.py

--- ch_2 (loops)/1_while_loop/scool_works/sw_72_001.py
+++ ch_2 (loops)/1_while_loop/scool_works/sw_72_001.py
@@ -1,6 +1,40 @@
-numb = 0
-while numb < 10:
-	if numb %2 == 1:
-		print(numb, "<===>", numb**2)
+# print("Kvela cifris kvadratebi")
+# n = 0
+# while n < 10:
+# 	print(n, "<===>", n**2)
+# 	n += 1    # n = n + 1
+
+
+
+# # --------------
+# input("\nDone!..")
+
+
+# print("Kvela kenti cifris kvadratebi")
+# n = 1
+# while n < 10:
+# 	print(n, "<===>", n**2)
+# 	n += 2    # n = n + 2
+
+
+# print("0-dan 10-mde kenti cifris kvadratebi")
+# n = 0
+# while n < 10:
+# 	if n%2 == 1:
+# 		print(n, "<===>", n**2)
 	
-	numb += 1
+# 	n += 1    # n = n + 1
+
+
+print("0-dan 10-mde kenti cifris kvadratebi")
+n = 0
+while 2*n+1 <= 9:
+	print(n+1, "<===>", (2*n+1)**2)
+	
+	n += 1    # n = n + 1
+
+
+
+
+# --------------
+input("\nDone!..")
