67757958cd31a715dbb7132813d5937e5a134e23
pm72 <pm72github@yahoo.com>
2019-02-08 11:33:39 +0400

    Create sw_73_001.py

---
+++ ch_2 (loops)/1_while_loop/scool_works/sw_73_001.py
@@ -0,0 +1,25 @@
+# print("Kvela cifris kvadratebi")
+# n = 0
+# while n < 10:
+# 	print(n, "<===>", n**2)
+# 	n += 1   # n = n + 1
+
+
+# print("Kvela kenti cifris kvadratebi")
+# n = 1
+# while n < 10:
+# 	print(n, "<===>", n**2)
+# 	n += 2   # n = n + 2\
+
+
+print("Kvela kenti cifris kvadratebi")
+n = 0
+while 2*n+1 < 10:
+	print(n+1, "<===>", n**2)
+	
+	n += 1   # n = n + 1
+
+
+
+# ---------------
+input("\nDone!..")
