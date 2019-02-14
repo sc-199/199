5eb7eaed9c3b7c85c38e83ae9012a4e926bacbd6
pm72 <pm72github@yahoo.com>
2019-02-14 13:00:58 +0400

    Update sw_72_001.py

--- ch_2 (loops)/1_while_loop/scool_works/sw_72_001.py
+++ ch_2 (loops)/1_while_loop/scool_works/sw_72_001.py
@@ -26,12 +26,35 @@
 # 	n += 1    # n = n + 1
 
 
-print("0-dan 10-mde kenti cifris kvadratebi")
-n = 0
-while 2*n+1 <= 9:
-	print(n+1, "<===>", (2*n+1)**2)
+# print("0-dan 10-mde kenti cifris kvadratebi")
+# n = 0
+# while 2*n+1 <= 9:
+# 	print(n+1, "<===>", (2*n+1)**2)
 	
-	n += 1    # n = n + 1
+# 	n += 1    # n = n + 1
+
+print("N-nishna ricvis ciprta jami")
+
+# # 52  52%10 + 52//10
+# n = 52
+# s = n % 10 + n // 10
+# print(s)
+# print(s, "=", n%10, "+", n//10)
+# print("{} = {} + {}".format(s, n%10, n//10))
+
+#943
+# n = 943
+# s = n%10 + (n//10)%10 + n//10//10
+# print(s)
+
+n = int(input("Enter integer value: "))
+s = 0
+# 9562
+while n != 0:		# 9562	956			95				9						0
+	s += n % 10		# 0+2		0+2+6		0+2+6+5		0+2+6+5+9
+	n //= 10			# 956		95			9					0
+
+print(s)
 
 
 
