f8f862a87ddb47dfb2232fa8ed4a3bfccf5b2cc1
pm72 <pm72github@yahoo.com>
2019-02-14 11:30:06 +0400

    Update sw_75_001.py

--- ch_2 (loops)/1_while_loop/scool_works/sw_75_001.py
+++ ch_2 (loops)/1_while_loop/scool_works/sw_75_001.py
@@ -27,5 +27,21 @@
 # 51		1 + 5 = 6
 # 			51%10    51//10
 
-n = 516
-print(n%10 + n//10)
+n = 51
+# print(n%10 + n//10)
+s = n%10 + n//10
+# print(s, "=", n%10, "+", n//10)
+# print("{} = {} + {}".format(s, n%10, n//10))
+
+# n = 579
+# s = n%10 + (n//10)%10 + n//10//10
+# print(s)
+
+#587
+# n = int(input("Enter integer value: "))
+s = 0
+while n != 0:
+	s += n%10			# 7			7+8		7+8+5
+	n //=10				# 58		5			0
+
+print(s)
