0fd574c67e6dd893ec4c36122365afaf8c097189
pm72 <pm72github@yahoo.com>
2019-02-15 11:35:09 +0400

    Update sw_73_001.py

--- ch_2 (loops)/1_while_loop/scool_works/sw_73_001.py
+++ ch_2 (loops)/1_while_loop/scool_works/sw_73_001.py
@@ -12,12 +12,35 @@
 # 	n += 2   # n = n + 2\
 
 
-print("Kvela kenti cifris kvadratebi")
-n = 0
-while 2*n+1 < 10:
-	print(n+1, "<===>", n**2)
+# print("Kvela kenti cifris kvadratebi")
+# n = 0
+# while 2*n+1 < 10:
+# 	print(n+1, "<===>", n**2)
 	
-	n += 1   # n = n + 1
+# 	n += 1   # n = n + 1
+
+
+print("N-nishna ricxvis ciprta jami")
+# 45   45%10 + 45//10
+# 825  825%10 + (825//10)%10  825//10//10
+
+n = 25
+s = n%10 + n//10
+print(s)
+print(n%10, "+", n//10, "=", s)
+print("{} = {} + {}".format(s, n%10, n//10))
+
+
+
+# n = int(input("Enter integer number: "))
+# s = 0
+# #4835
+# while n != 0:		# 4835	483			48				4						0
+# 	s += n%10			# 0+5		0+5+3		0+5+3+8		0+5+3+8+4
+# 	n //= 10			# 483		48			4					0
+
+# print(s)
+
 
 
 
